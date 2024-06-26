afcfd3ddaeb12e05c58fa65212f9433ad1a116e5
Daniel Tomé Fernández <danieltomefer@gmail.com>
2024-06-26 08:53:10 +0200

    More work

--- FoundryAdvanced/ERC721Nft/script/Interactions.s.sol
+++ FoundryAdvanced/ERC721Nft/script/Interactions.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 contract MintBasicNft is Script {
     string public constant PUG =
@@ -18,6 +19,34 @@ contract MintBasicNft is Script {
         vm.startBroadcast();
         BasicNft basicNft = BasicNft(contractAddress);
         basicNft.mintNFT(PUG);
+        vm.stopBroadcast();
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) private {
+        vm.startBroadcast();
+        MoodNft moodNft = MoodNft(contractAddress);
+        moodNft.mintNft();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MoodNft", block.chainid);
+        flipMoodNft(mostRecentlyDeployed);
+    }
+
+    function flipMoodNft(address contractAddress) private {
+        vm.startBroadcast();
+        MoodNft moodNft = MoodNft(contractAddress);
+        moodNft.flipMood(0);
         vm.stopBroadcast();
     }
 }
